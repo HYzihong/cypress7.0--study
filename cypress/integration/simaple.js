b4bc24ee1f1824c9d33ac4e1c0b0c1bd8766d509
hy <zihong2020@foxmail.com>
2021-05-27 10:29:12 +0800

    feat(cypress) add cypress

---
+++ cypress/integration/simaple.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: your name
+ * @Date: 2021-05-27 10:16:44
+ * @LastEditTime: 2021-05-27 10:20:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /cypress-study/cypress/integration/simaple.js
+ */
+describe("My First Test", () => {
+  it("Does not do much!", () => {
+    expect(true).to.equal(false);
+  });
+});
